df26303b69f1a7e4fbb7c88576b5d937913383ed
Ever Joel Ccencho Santacruz <110436144+EvryDash05@users.noreply.github.com>
2025-06-29 02:53:23 -0500

    Se limpia el csv flipkart

--- data_lakehouse/utils/helpers.py
+++ data_lakehouse/utils/helpers.py
@@ -1,4 +1,5 @@
 from pandas import DataFrame
+from numpy import nan
 from config.constants import VALID_ROUTES
 import os
 
@@ -11,3 +12,16 @@ def save_on_data_lake(route: str, df: DataFrame, dataset_name: str):
 
     parquet_route = str(f"{route}/{dataset_name}")
     df.to_parquet(parquet_route, index=False, engine="pyarrow")
+    
+# Función para remplazar vacios por verdaderos nulos
+def replace_null_values(dataframe: DataFrame):
+    null_values = ['None', 'N/A', 'NA', '', ' ', 'null', 'nan']
+    # Se usa el inplace para modificar el dataframe original
+    return dataframe.replace(null_values, nan, inplace=True)
+
+# Devuelve la cantidad de valores nan por columna
+def verified_nan_values(dataFrame: DataFrame):
+    columns = dataFrame.columns
+    for column in columns:
+        nan_qt = dataFrame[column].isna().sum()
+        print(f'Cantidad de valores NaN en {column}: {nan_qt}')
